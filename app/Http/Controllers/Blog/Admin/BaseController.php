c4e6b9a39d3a100d5c0df931201125c8d69c27fd
parabellum <impala67dean666@gmail.com>
2020-06-01 22:53:40 +0300

    Add first controllers

---
+++ app/Http/Controllers/Blog/Admin/BaseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Blog\BaseController;
+
+class BaseController extends BaseController
+{
+    //
+}
