5d840775c55adbab5ed39ebb72a315739035641d
parabellum <impala67dean666@gmail.com>
2020-06-03 23:28:38 +0300

    Some category includes moved to admin/includes

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -6,15 +6,15 @@
             @method('PATCH')
             @csrf
             <div class="container">
-                @include('blog.admin.categories.includes.display_action_status')
+                @include('blog.admin.includes.display_action_status')
 
                 <div class="row justify-content-center">
                     <div class="col-md-7">
                         @include('blog.admin.categories.includes.item_filling_data')
                     </div>
                     <div class="col-md-4">
-                        @include('blog.admin.categories.includes.item_save_data')
-                        @include('blog.admin.categories.includes.item_edit_secondary_data')
+                        @include('blog.admin.includes.item_save_data')
+                        @include('blog.admin.includes.item_edit_secondary_data')
                     </div>
                 </div>
             </div>
