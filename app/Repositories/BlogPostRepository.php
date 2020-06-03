d66a5ffac577c92129a084aa09590a8b206267f3
parabellum <impala67dean666@gmail.com>
2020-06-03 23:55:44 +0300

    Add BlogPostRepository

---
+++ app/Repositories/BlogPostRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogPost;
+
+/**
+ * Class BlogPostRepository.
+ */
+class BlogPostRepository extends CoreRepository
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    protected function getModelClass()
+    {
+        return BlogPost::class;
+    }
+
+    public function getAllWithPaginate($perPage = null)
+    {
+        $columns = [
+            'id',
+            'title',
+            'user_id',
+            'category_id',
+            'published_at',
+            'is_published',
+        ];
+
+        $result = $this->startConditions()
+            ->select($columns)
+            ->orderBy('id', 'DESC')
+            ->with([
+                'category:id,title',
+                'user:id,name',
+            ])->paginate($perPage);
+
+        return $result;
+    }
+
+    public function getEdit($id)
+    {
+        return $this->startConditions()->find($id);
+    }
+}
--- resources/views/blog/admin/categories/includes/item_delete.blade.php
+++ resources/views/blog/admin/categories/includes/item_delete.blade.php
@@ -1,11 +1,3 @@
-{{--<div class="row justify-content-center">--}}
-{{--    <div class="col-md-8">--}}
-{{--        <div class="card">--}}
-{{--            <div class="card-body">--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
 <div class="row justify-content-center">
     <div class="col-md-3">
         <div class="card">
