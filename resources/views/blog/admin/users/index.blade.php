be9f1bd1b4aadf58c132c46ba7c019d189ce3f85
parabellum <impala67dean666@gmail.com>
2020-06-16 19:09:09 +0300

    Add action result to users admin index view

--- resources/views/blog/admin/users/index.blade.php
+++ resources/views/blog/admin/users/index.blade.php
@@ -4,6 +4,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-10 col-lg-10 col-sm-12">
+
+                @include('blog.admin.includes.display_action_status')
+
                 <div class="card">
                     <div class="card-body">
                         <table class="table table-hover">
