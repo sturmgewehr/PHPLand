947bddcf4218f36af8b7ed991db8e2ccd4616932
parabellum <impala67dean666@gmail.com>
2020-06-21 17:18:55 +0300

    Add UserService

---
+++ app/Services/UserService.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Repositories\UserRepository;
+
+class UserService extends BaseService
+{
+    /**
+     * @var UserRepository
+     */
+    private $repository;
+
+    public function __construct()
+    {
+        $this->repository = app(UserRepository::class);
+    }
+
+    protected function isExist($item)
+    {
+        if(empty($item))
+        {
+            abort(404);
+        }
+    }
+
+    public function getAllWithPaginate($perPage = null)
+    {
+        return $this->repository->getAllWithPaginate($perPage);
+    }
+
+    public function create(array $input)
+    {
+        $result = $this->repository->create($input);
+
+        $this->isExist($result);
+
+        return $result;
+    }
+
+    public function update($id, array $input)
+    {
+        $item = $this->repository->update($id, $input);
+
+        $this->isExist($item);
+
+        return $item;
+    }
+
+    public function getEdit($id)
+    {
+        $item = $this->repository->getEdit($id);
+
+        $this->isExist($item);
+
+        return $item;
+    }
+
+    public function destroy($id)
+    {
+        return $this->repository->destroy($id);
+    }
+}
