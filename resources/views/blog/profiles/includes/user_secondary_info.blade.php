86350889eac8954c4e321b4c7d67a93282216279
parabellum <impala67dean666@gmail.com>
2020-09-24 11:55:54 +0300

    Add view for searched items

--- resources/views/blog/profiles/includes/user_secondary_info.blade.php
+++ resources/views/blog/profiles/includes/user_secondary_info.blade.php
@@ -3,7 +3,8 @@
         <div class="card">
             <div class="card-body text-center">
                 <button class="btn btn-light border border-dark">
-                    <a class="nav-link simple-link" href="">Написанные статьи</a>
+                    <a class="nav-link simple-link"
+                       href="{{ route('search_route', ['user_id', $user['user']['id']]) }}">Написанные статьи</a>
                 </button>
             </div>
         </div>
